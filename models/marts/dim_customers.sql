8a84a84363a702b790b444988a3fe93512fdc211
Bianca Hernandez <bianca.hernandez@live.com>
2026-05-21 04:36:50 +0000

    created the fact orders mart in the finance subdirectory. added an aggregation for lifetime customer value to the dim_customers model.

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -10,6 +10,11 @@ select * from {{ ref('stg_jaffle_shop__orders') }}
 
 ),
 
+payments as(
+
+    select * from {{ ref('stg_stripe__payments')}}
+),
+
 customer_orders as (
 
     select
@@ -24,6 +29,18 @@ customer_orders as (
 
 ),
 
+customer_lifetime_value as (
+    select
+    orders.customer_id,
+    sum(payments.amount) as lifetime_value
+
+    from payments
+    left join orders on payments.order_id = orders.order_id
+
+    group by 1
+
+),
+
 
 final as (
 
@@ -33,11 +50,13 @@ final as (
         customers.last_name,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
+        coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
+        customer_lifetime_value.lifetime_value
 
     from customers
 
     left join customer_orders using (customer_id)
+    left join customer_lifetime_value using (customer_id)
 
 )
 
